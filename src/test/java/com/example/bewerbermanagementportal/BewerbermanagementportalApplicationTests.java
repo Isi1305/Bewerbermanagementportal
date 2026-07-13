7b18f139a99bb0a3202e2d92f905cba8ec2993a0
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-13 09:39:05 +0200

    Initial commit

---
+++ src/test/java/com/example/bewerbermanagementportal/BewerbermanagementportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bewerbermanagementportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BewerbermanagementportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
