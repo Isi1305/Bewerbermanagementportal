dfee29eb68fc1c944a2a147ce8ea96547ead467e
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-16 15:20:32 +0200

    name, email, passwort und Rolle hinzugefügt.

--- src/main/java/com/example/bewerbermanagementportal/entity/Nutzer.java
+++ src/main/java/com/example/bewerbermanagementportal/entity/Nutzer.java
@@ -1,15 +1,16 @@
 package com.example.bewerbermanagementportal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.persister.entity.UniqueKeyEntry;
 
 @Entity
 public class Nutzer {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // automatische Hochzählung
+    @Id // makiert das Feld als Primärschlüssel
     private Long id;
+
+    public Nutzer() {
+    }
 
     public void setId(Long id) {
         this.id = id;
@@ -18,4 +19,17 @@ public class Nutzer {
     public Long getId() {
         return id;
     }
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String passwort;
+
+    @Enumerated(EnumType.STRING) // Speichert den Wert als Text
+    @Column(nullable = false)
+    private Rolle rolle; // Typ Rolle: selbstgebautes Enum
 }
