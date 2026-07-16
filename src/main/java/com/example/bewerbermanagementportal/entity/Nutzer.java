b57d2eae1efe653ae06e454534d3d862db14f01d
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-16 13:34:01 +0200

    id vollständig als Primärschlüssel mit automatischer Hochzählung eingerichtet.

--- src/main/java/com/example/bewerbermanagementportal/entity/Nutzer.java
+++ src/main/java/com/example/bewerbermanagementportal/entity/Nutzer.java
@@ -1,8 +1,21 @@
 package com.example.bewerbermanagementportal.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Nutzer {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
