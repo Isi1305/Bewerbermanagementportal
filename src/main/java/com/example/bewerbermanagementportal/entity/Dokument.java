41e40a97e795a9bf8bfe439b93e43e6ce01572de
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-20 15:22:06 +0200

    In Dokument alle Getter-Setter sowie Enum hinzugefügt.

--- src/main/java/com/example/bewerbermanagementportal/entity/Dokument.java
+++ src/main/java/com/example/bewerbermanagementportal/entity/Dokument.java
@@ -1,6 +1,7 @@
 package com.example.bewerbermanagementportal.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Dokument {
@@ -15,12 +16,76 @@ public class Dokument {
     private String name;
 
     @Column(nullable = false)
-    private Date datum;
+    private LocalDate uploadDatum;
 
     @Column(nullable = false, length = 250)
     private String dateipfad;
 
     public enum DokumentTyp {
         PDF, DOCX, JPG, TXT
+    }
+
+    // Nutzung von Dokumenttyp Enum
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DokumentTyp dokumenttyp;
+
+    // Getter-Setter für id
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    // Getter_Setter für name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Getter-Setter für uploadDatum
+    public void setUploadDatum(LocalDate uploadDatum) {
+        this.uploadDatum = uploadDatum;
+    }
+
+    public LocalDate getUploadDatum() {
+        return uploadDatum;
+    }
+
+    // Getter-Setter für dateipfad
+    public void setDateipfad(String dateipfad) {
+        this.dateipfad = dateipfad;
+    }
+
+    public String getDateipfad() {
+        return dateipfad;
+    }
+
+    // Getter-Setter füt dokumenttyp
+    public void setDokumentTyp(DokumentTyp dokumenttyp) {
+        this.dokumenttyp = dokumenttyp;
+    }
+
+    public DokumentTyp getDokumentTyp() {
+        return dokumenttyp;
+    }
+
+    // Beziehung zu Bewerbung
+    @ManyToOne
+    @JoinColumn(name ="bewerbung_id", nullable = false)
+    private Bewerbung bewerbung;
+
+    // Getter-Setter für bewerbung
+    public void setBewerbung(Bewerbung bewerbung) {
+        this.bewerbung = bewerbung;
+    }
+
+    public Bewerbung getBewerbung() {
+        return bewerbung;
     }
 }
