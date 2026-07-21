6d5cf35a3648b99647ec01f132489ced4e28b7ab
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-21 15:00:32 +0200

    Registrier-Logik hinzugefügt.

--- src/main/java/com/example/bewerbermanagementportal/service/NutzerService.java
+++ src/main/java/com/example/bewerbermanagementportal/service/NutzerService.java
@@ -1,5 +1,6 @@
 package com.example.bewerbermanagementportal.service;
 
+import com.example.bewerbermanagementportal.entity.Nutzer;
 import com.example.bewerbermanagementportal.repository.NutzerRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,12 @@ public class NutzerService {
     private final NutzerRepository nutzerRepository; // speichert das Repository und ändert sich nicht mehr.
     public NutzerService(NutzerRepository nutzerRepository) {
         this.nutzerRepository = nutzerRepository;
+    }
+
+    public Nutzer registrieren(Nutzer nutzer) { // nimmt Wert entgegen und gibt ihn am Ende gespeichert zurück.
+        if (nutzerRepository.existsByEmail(nutzer.getEmail())) {
+            throw new IllegalArgumentException("E-Mail bereits registriert"); // bricht die Methode sofort ab und meldet einen Fehler, falls die E-Mail schon existiert.
+        }
+        return nutzerRepository.save(nutzer);
     }
 }
