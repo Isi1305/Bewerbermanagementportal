7b4ca17b8fde67ec9eee0ddf9991b6272a3883a1
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-21 09:56:37 +0200

    existsByEmail in NutzerRepository.

--- src/main/java/com/example/bewerbermanagementportal/repository/NutzerRepository.java
+++ src/main/java/com/example/bewerbermanagementportal/repository/NutzerRepository.java
@@ -4,4 +4,5 @@ import com.example.bewerbermanagementportal.entity.Nutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NutzerRepository extends JpaRepository<Nutzer, Long> {
+    boolean existsByEmail(String email); // prüft, ob ein Datensatz mit einem email-Wert existiert
 }
