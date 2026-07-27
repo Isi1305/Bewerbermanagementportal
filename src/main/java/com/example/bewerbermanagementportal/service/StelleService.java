b7cf84d1f457c86fe12cc054d4b769d7620a3c5e
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-27 13:31:37 +0200

    Stelle entgegennehmen, speichern und zurückgeben

--- src/main/java/com/example/bewerbermanagementportal/service/StelleService.java
+++ src/main/java/com/example/bewerbermanagementportal/service/StelleService.java
@@ -1,4 +1,19 @@
 package com.example.bewerbermanagementportal.service;
 
+import com.example.bewerbermanagementportal.entity.Stelle;
+import com.example.bewerbermanagementportal.repository.StelleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StelleService {
+    private final StelleRepository stelleRepository;
+
+    public StelleService(StelleRepository stelleRepository) {
+        this.stelleRepository = stelleRepository;
+    }
+
+    // Stelle entgegennehmen, speichern und zurückgeben
+    public Stelle stelleAnlegen(Stelle stelle) {
+        return stelleRepository.save(stelle);
+    }
 }
