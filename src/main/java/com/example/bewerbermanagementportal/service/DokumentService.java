8d272dbc0940fcb38ed16a07006512fa18b56af8
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-28 13:49:20 +0200

    alle Dokumente zu einer Bewerbung anzeigen

--- src/main/java/com/example/bewerbermanagementportal/service/DokumentService.java
+++ src/main/java/com/example/bewerbermanagementportal/service/DokumentService.java
@@ -1,7 +1,11 @@
 package com.example.bewerbermanagementportal.service;
 
+import com.example.bewerbermanagementportal.entity.Bewerbung;
+import com.example.bewerbermanagementportal.entity.Dokument;
 import com.example.bewerbermanagementportal.repository.DokumentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DokumentService {
@@ -9,5 +13,15 @@ public class DokumentService {
 
     public DokumentService(DokumentRepository dokumentRepository) {
         this.dokumentRepository = dokumentRepository;
+    }
+
+    // Dokument entgegennehmen, speichern und zurückgeben
+    public Dokument dokumentSpeichern(Dokument dokument) {
+        return dokumentRepository.save(dokument);
+    }
+
+    // alle Dokumente zu einer Bewerbung anzeigen
+    public List<Dokument> dokumenteProBewerbung(Bewerbung bewerbung) {
+        return dokumentRepository.findByBewerbung(bewerbung);
     }
 }
