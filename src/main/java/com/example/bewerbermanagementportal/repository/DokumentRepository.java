8abff389c5b52889d9c9620e5a30990a63e3646c
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-28 13:44:18 +0200

    Liste in DokumentRepository

--- src/main/java/com/example/bewerbermanagementportal/repository/DokumentRepository.java
+++ src/main/java/com/example/bewerbermanagementportal/repository/DokumentRepository.java
@@ -1,7 +1,11 @@
 package com.example.bewerbermanagementportal.repository;
 
+import com.example.bewerbermanagementportal.entity.Bewerbung;
 import com.example.bewerbermanagementportal.entity.Dokument;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DokumentRepository extends JpaRepository<Dokument, Long> {
+    List<Dokument> findByBewerbung(Bewerbung bewerbung);
 }
