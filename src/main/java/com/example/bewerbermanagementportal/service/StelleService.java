9a76d3cb422f0b390c5538959fa0b72c5c017c69
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-28 10:30:08 +0200

    Einzelne Stellen können jetzt per ID gefunden werden. Fehlermeldung wenn es nicht existiert.

--- src/main/java/com/example/bewerbermanagementportal/service/StelleService.java
+++ src/main/java/com/example/bewerbermanagementportal/service/StelleService.java
@@ -5,6 +5,7 @@ import com.example.bewerbermanagementportal.repository.StelleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StelleService {
@@ -22,5 +23,16 @@ public class StelleService {
     // Liste aller Stellen ausgeben
     public List<Stelle> alleStellenAnzeigen() {
         return stelleRepository.findAll();
+    }
+
+    // einzelne Stelle per ID finden
+    public Stelle stelleFindenPerId(Long id) {
+        Optional<Stelle> gefundeneStelle = stelleRepository.findById(id);
+
+        if (gefundeneStelle.isEmpty()) {
+            throw new IllegalArgumentException("Stelle existiert nicht");
+        }
+
+        return gefundeneStelle.get();
     }
 }
