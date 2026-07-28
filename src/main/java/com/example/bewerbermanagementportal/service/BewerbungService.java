fa65b52872126f47df9745e5a9729522423670cd
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-28 11:38:50 +0200

    Status Änderung.

--- src/main/java/com/example/bewerbermanagementportal/service/BewerbungService.java
+++ src/main/java/com/example/bewerbermanagementportal/service/BewerbungService.java
@@ -1,7 +1,11 @@
 package com.example.bewerbermanagementportal.service;
 
+import com.example.bewerbermanagementportal.entity.Bewerbung;
+import com.example.bewerbermanagementportal.entity.BewerbungStatus;
 import com.example.bewerbermanagementportal.repository.BewerbungRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BewerbungService {
@@ -9,5 +13,24 @@ public class BewerbungService {
 
     public BewerbungService(BewerbungRepository bewerbungRepository) {
         this.bewerbungRepository = bewerbungRepository;
+    }
+
+    // Status kommt rein als EINGANG
+    public Bewerbung bewerbungEinreichen(Bewerbung bewerbung) {
+        bewerbung.setStatus(BewerbungStatus.EINGANG);
+        return bewerbungRepository.save(bewerbung);
+    }
+
+    // Status Umwandlung und suche nach ID
+    public Bewerbung statusAendern(Long id, BewerbungStatus neuerStatus) {
+        Optional<Bewerbung> gefundeneBewerbung = bewerbungRepository.findById(id);
+
+        if (gefundeneBewerbung.isEmpty()) {
+            throw new IllegalArgumentException("Bewerbung existiert nicht");
+        }
+
+        gefundeneBewerbung.get().setStatus(neuerStatus);
+
+        return bewerbungRepository.save(gefundeneBewerbung.get());
     }
 }
