5ce5a30be00f19be794328db1d825e5522c2fd18
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-29 12:46:47 +0200

    NutzerController abgeschlossen. Login und Registrier Formulardaten werden gespeichert.

--- src/main/java/com/example/bewerbermanagementportal/controller/NutzerController.java
+++ src/main/java/com/example/bewerbermanagementportal/controller/NutzerController.java
@@ -1,7 +1,11 @@
 package com.example.bewerbermanagementportal.controller;
 
+import com.example.bewerbermanagementportal.entity.Nutzer;
 import com.example.bewerbermanagementportal.service.NutzerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NutzerController {
@@ -9,5 +13,30 @@ public class NutzerController {
 
     public NutzerController(NutzerService nutzerService) {
         this.nutzerService = nutzerService;
+    }
+
+    // leerers Formular, deswegen müssen keine Daten hier übergeben werden
+    @GetMapping("/registrieren")
+    public String registrierenFormular() {
+        return "registrieren";
+    }
+
+    // Formulardaten atomatisch in ein Nutzer-Objekt
+    @PostMapping("/registrieren")
+    public String registrieren(@ModelAttribute Nutzer nutzer) {
+        nutzerService.registrieren(nutzer);
+        return "redirect:/login";
+    }
+
+    // leeres Formular bei login
+    @GetMapping("/login")
+    public String loginFormular() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Nutzer nutzer) {
+        nutzerService.login(nutzer);
+        return "redirect:/startseite";
     }
 }
