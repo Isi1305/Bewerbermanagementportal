7556ec449b6ac10ca44f6849c99d44be3ba078b3
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-29 11:48:09 +0200

    StelleController fertig. Jetzt kann eine einzelne Stelle angezeigt werden.

--- src/main/java/com/example/bewerbermanagementportal/controller/StelleController.java
+++ src/main/java/com/example/bewerbermanagementportal/controller/StelleController.java
@@ -4,6 +4,7 @@ import com.example.bewerbermanagementportal.service.StelleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class StelleController {
@@ -18,5 +19,12 @@ public class StelleController {
     public String alleStellen(Model model) {
         model.addAttribute("stellen", stelleService.alleStellenAnzeigen());
         return "stellen-liste"; // Name der Thymeleaf-Datei Platzhalter
+    }
+
+    // einzelne Stelle anzeigen
+    @GetMapping("/stellen/{id}")
+    public String stelleDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("stelle", stelleService.stelleFindenPerId(id));
+        return "stelle-detail";
     }
 }
