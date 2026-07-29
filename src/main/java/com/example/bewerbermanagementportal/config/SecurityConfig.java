7787cfc7d350ae54c869813aea3ff7f2933132da
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-29 13:28:29 +0200

    Teile des Portals öffentlich gestellt, damit ich was sehe.

--- src/main/java/com/example/bewerbermanagementportal/config/SecurityConfig.java
+++ src/main/java/com/example/bewerbermanagementportal/config/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.example.bewerbermanagementportal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Teile des Portals öffentlich stellen
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/stellen", "/stellen/**", "/registrieren", "/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
