545c1deb79a6cfd8477406c414fdcf5c25173cd3
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-30 15:01:13 +0200

    Platzhalter ist weg und Login wird sich gemerkt.

--- src/main/java/com/example/bewerbermanagementportal/controller/BewerbungController.java
+++ src/main/java/com/example/bewerbermanagementportal/controller/BewerbungController.java
@@ -2,7 +2,12 @@ package com.example.bewerbermanagementportal.controller;
 
 import com.example.bewerbermanagementportal.entity.Bewerbung;
 import com.example.bewerbermanagementportal.entity.BewerbungStatus;
+import com.example.bewerbermanagementportal.entity.Nutzer;
+import com.example.bewerbermanagementportal.entity.Stelle;
+import com.example.bewerbermanagementportal.repository.NutzerRepository;
+import com.example.bewerbermanagementportal.repository.StelleRepository;
 import com.example.bewerbermanagementportal.service.BewerbungService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,16 +17,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class BewerbungController {
     private final BewerbungService bewerbungService;
+    private final NutzerRepository nutzerRepository;
+    private final StelleRepository stelleRepository;
 
-    public BewerbungController(BewerbungService bewerbungService) {
+    public BewerbungController(BewerbungService bewerbungService, NutzerRepository nutzerRepository, StelleRepository stelleRepository) {
         this.bewerbungService = bewerbungService;
+        this.nutzerRepository = nutzerRepository;
+        this.stelleRepository = stelleRepository;
     }
 
     // Einreichen der Bewerbung und Weiterleitung zurück zur Stellendetailseite
     @PostMapping("/bewerbung")
-    public String bewerbungEinreichen(@ModelAttribute Bewerbung bewerbung) {
+    public String bewerbungEinreichen(@RequestParam Long stelleId, HttpSession session) {
+        Long nutzerId = (Long) session.getAttribute("nutzerId");
+        Nutzer bewerber = nutzerRepository.findById(nutzerId).get();
+        Stelle stelle = stelleRepository.findById(stelleId).get();
+
+        Bewerbung bewerbung = new Bewerbung();
+        bewerbung.setBewerber(bewerber);
+        bewerbung.setStelle(stelle);
+
         bewerbungService.bewerbungEinreichen(bewerbung);
-        return "redirect:/stelle-detail";
+        return "redirect:/stellen/" + stelleId;
     }
 
     // Statusänderung vom Recruiter
