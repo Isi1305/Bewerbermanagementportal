203a586eda68508829c2b4ad09ee2457dcac4f19
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-31 18:43:53 +0200

    Fester Recruiter erstellt wegen H2.

--- src/main/java/com/example/bewerbermanagementportal/DataInitializer.java
+++ src/main/java/com/example/bewerbermanagementportal/DataInitializer.java
@@ -1,6 +1,9 @@
 package com.example.bewerbermanagementportal;
 
+import com.example.bewerbermanagementportal.entity.Nutzer;
+import com.example.bewerbermanagementportal.entity.Rolle;
 import com.example.bewerbermanagementportal.entity.Stelle;
+import com.example.bewerbermanagementportal.repository.NutzerRepository;
 import com.example.bewerbermanagementportal.repository.StelleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,15 +13,25 @@ public class DataInitializer implements CommandLineRunner {
 
 
     private final StelleRepository stelleRepository;
+    private final NutzerRepository nutzerRepository;
 
-    public DataInitializer(StelleRepository stelleRepository) {
+    public DataInitializer(StelleRepository stelleRepository, NutzerRepository nutzerRepository) {
         this.stelleRepository = stelleRepository;
+        this.nutzerRepository = nutzerRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         System.out.println("Testdaten werden geladen");
+
+        Nutzer recruiter = new Nutzer();
+        recruiter.setName("Max Musterman");
+        recruiter.setEmail("recruiter@text.de");
+        recruiter.setPasswort("1234");
+        recruiter.setRolle(Rolle.RECRUITER);
+
+        nutzerRepository.save(recruiter);
 
         Stelle stelle = new Stelle();
         stelle.setTitel("Java Entwickler");
