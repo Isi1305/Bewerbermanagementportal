5624b5cb73d1ab455d019749bd566150ced9eb33
Isi1305 <isabell.sophieandrea@gmail.com>
2026-07-31 08:45:45 +0200

    stelle-neu.html und StelleRepository erweitert für das DropDown.

--- src/main/java/com/example/bewerbermanagementportal/repository/StelleRepository.java
+++ src/main/java/com/example/bewerbermanagementportal/repository/StelleRepository.java
@@ -1,7 +1,14 @@
 package com.example.bewerbermanagementportal.repository;
 
+import com.example.bewerbermanagementportal.entity.Karrierestatus;
 import com.example.bewerbermanagementportal.entity.Stelle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StelleRepository extends JpaRepository<Stelle, Long> {
+    List<Stelle> findByArbeitsbereichAndKarrierstatus(
+            String arbeitsbereich,
+            Karrierestatus karrierestatus
+    );
 }
